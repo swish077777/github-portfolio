3ce241aa6596f1992e7928778581fcd014396757
swish077777 <122419407+swish077777@users.noreply.github.com>
2023-11-30 10:17:59 +0800

    Add files via upload

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,27 +1,45 @@
-<script>
-  $(document).ready(function () {
-    // Handle the slide.bs.carousel event
-    $('#carouselExample1').on('slide.bs.carousel', function (e) {
-      var currentIndex = $(e.relatedTarget).index();
-      updateTextBasedOnSlide(currentIndex);
-    });
-
-    function updateTextBasedOnSlide(index) {
-      // You can implement your logic to change text based on the current slide index here
-      switch (index) {
+{/* <script>
+       $(document).ready(function () {
+       $('#carouselExampleFade').on('slide.bs.carousel', function (e) {
+        var currentIndex = $(e.relatedTarget).index();
+          updateTextBasedOnSlide(currentIndex);
+              });
+          
+       function updateTextBasedOnSlide(index) {
+       switch (index) {
         case 0:
-          $('#carouselTextTitle').text('Frontgate');
-          $('#carouselTextDescription').text('Upon entering through the main gate, individuals can effortlessly proceed along a direct path, continuing until they reach the second staircase positioned directly in front of the Beta building. It is at this point that they will find the Clinic, conveniently located and easily accessible.');
+          $('#carouselTextTitle').text('Computer Hardware Basics');
+          $('#carouselTextDescription').text('Cisco verifies the earner of this badge successfully completed the Computer Hardware Basics course and achieved this student level credential. Earner has fundamentals knowledge of computers and mobile devices, how they work, as well as the basic concepts and skills needed to install components to build, repair, upgrade personal computers and and basic troubleshooting tools and techniques.');
           break;
         case 1:
-
-        $('#carouselTextTitle').text('Frontgate');
-          $('#carouselTextDescription').text('loremwerwersdfo  wehniweur uweriuwerh');
-          // Change text for the second slide
+          $('#carouselTextTitle').text('Google Data Analytics');
+          $('#carouselTextDescription').text('Those who earn the Google Data Analytics Certificate have completed eight courses, developed by Google, that include hands-on, practice-based assessments and are designed to prepare them for introductory-level roles in Data Analytics. They are competent in tools and platforms including spreadsheets, SQL, Tableau, and R. They know how to prepare, process, analyze, and share data for thoughtful action.');
           break;
-        // Add cases for other slides as needed
-      }
-    }
-  });
-
-  </script>
+         case 2:
+          $('#carouselTextTitle').text('Google IT Automation with Python');
+          $('#carouselTextDescription').text('This six-course certificate, developed by Google, is designed to provide IT professionals with in-demand skills -- including Python, Git, and IT automation -- that can help them advance their careers. The hands-on curriculum is designed to teach learners how to write code in Python, with a special focus on how this applies to automating tasks in the world of IT support and systems administration.');
+            break;
+        case 3:
+          $('#carouselTextTitle').text('Google IT Support');
+          $('#carouselTextDescription').text('Those who earn the Google IT Support Certificate have completed five-courses, developed by Google, that include hands-on, practice-based assessments and are designed to prepare them for entry-level roles in IT support. They are competent in foundational skills, including troubleshooting and customer service, networking, operating systems, system administration, and security.');
+            break;
+        case 4:
+          $('#carouselTextTitle').text('Introduction to Cybersecurity');
+          $('#carouselTextDescription').text('Cisco verifies the earner of this badge successfully completed the Introduction to Cybersecurity course. The holder of this student-level credential has introductory knowledge of cybersecurity, including the global implications of cyber threats on industries, and why cybersecurity is a growing profession. They understand vulnerabilities and threat detection and defense. They also have insight into opportunities available with pursuing cybersecurity certifications.');
+            break;
+        case 5:
+          $('#carouselTextTitle').text('Introduction to Internet of things');
+          $('#carouselTextDescription').text('Cisco verifies the earner of this badge successfully completed the Introduction to Internet of Things course. The holder of this student-level credential has introductory knowledge of IoT and has an understanding how it enables the Digital Transformation along with emerging technologies such as data analytics, AI/ML and the increased attention on cybersecurity. They understand the importance of Intent Based Networking to be able to connect and secure tens of billions of new devices with ease.');
+            break;
+        case 6:
+          $('#carouselTextTitle').text('Networking Basics');
+          $('#carouselTextDescription').text('Cisco verifies the earner of this badge successfully completed the Networking Basics course and achieved this student level credential. Earner has knowledge of the types of networks, how they work, how devices send and receive data, the types of network cabling, how IP addresses find information on the Internet, how transport and applications operate, and has practiced building a home wireless network. Participated in up to 13 Cisco Packet Tracer activities.');
+            break;
+        case 7:
+          $('#carouselTextTitle').text('Operating System Basics');
+          $('#carouselTextDescription').text('Cisco verifies the earner of this badge successfully completed the Operating Systems Basics course and achieved this student level credential. Earner has fundamental knowledge of operating systems by covering the basic concepts and skills needed to explain the purpose and characteristics of operating systems, implement basic operating system security, and explain how to configure mobile device network connectivity and email.');
+            break;
+                }
+              }
+            });
+          </script> */}
